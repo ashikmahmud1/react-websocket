3d8b52a3224704375a25cfee8eacd612c714d46c
Ashik Mahmud <ashikmahmud137@gmail.com>
2020-06-18 20:33:20 +0600

    Initial commit

---
+++ src/SocketConnection.js
@@ -0,0 +1,5 @@
+
+//    String plainCredentials = username + ":" + password;
+//    String base64Credentials = Base64.getEncoder().encodeToString(plainCredentials.getBytes());
+//    final WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+//    headers.add("Authorization", "Basic " + base64Credentials);
